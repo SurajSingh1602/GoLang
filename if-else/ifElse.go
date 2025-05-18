b640be226bee446c774f74a7033092cc10fc36ef
surajkant singh <suraj1621singh@gmail.com>
2025-05-18 19:13:06 +0530

    if-else done

--- if-else/ifElse.go
+++ if-else/ifElse.go
@@ -2,9 +2,23 @@ package main
 import "fmt"
 
 func main(){
-	age:= 19
+	// age:= 19
 
-	if age>=18{
-		fmt.Println("Legal to vote")
+	// if age>=18{
+	// 	fmt.Println("person is an adult")
+	// } else if age>=12{
+	// 	fmt.Println("Person is Teenager")
+	// } else{
+	// 	fmt.Println("Person is kid")
+	// }
+
+
+	var role = "admin"
+	var hasPermission = true
+
+	if role == "admin" && hasPermission{
+		fmt.Println("Can Access")
+	} else {
+		fmt.Println("Cannot Access the data")
 	}
 }	
